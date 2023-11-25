1998c5f3e2b169128344ea0ada25e342e5ecd514
Michal D <dvorkam@gmail.com>
2023-11-25 15:14:38 +0100

    applied formater

--- playground/foo.test.cpp
+++ playground/foo.test.cpp
@@ -1,8 +1,9 @@
 #include <gtest/gtest.h>
 
 #include <iostream>
-//import <iostream>;
-TEST(playground, pass_test) {
-    std::cout<<"hello world"<<std::endl;
+// import <iostream>;
+TEST(playground, pass_test)
+{
+    std::cout << "hello world" << std::endl;
     ASSERT_TRUE(true);
 }
--- randEngine/GeneratorsPredefined.test.cpp
+++ randEngine/GeneratorsPredefined.test.cpp
@@ -1,7 +1,7 @@
-//#include <iostream>
-//#include <memory>
-//#include <limits>
-//#include <random>
+// #include <iostream>
+// #include <memory>
+// #include <limits>
+// #include <random>
 
 #include <gtest/gtest.h>
 
@@ -13,8 +13,8 @@ import <memory>;
 import <limits>;
 import <random>;
 
-
-TEST(diceLibrary_generic, gtest_GeneratorsPredefined_integration) {
+TEST(diceLibrary_generic, gtest_GeneratorsPredefined_integration)
+{
     // Your test code goes here
     ASSERT_TRUE(true);
 }
@@ -22,67 +22,76 @@ TEST(diceLibrary_generic, gtest_GeneratorsPredefined_integration) {
 using GeneratorTypes = ::testing::Types<
     generators::GeneratorMt19937,
     generators::GeneratorMt19937_64,
-    generators::GeneratorGeneric<generators::DumbGenerator>
->;
+    generators::GeneratorGeneric<generators::DumbGenerator>>;
 
 using UniformRandomNumberGeneratorConceptTypes = ::testing::Types<
     std::mt19937,
     std::mt19937_64,
-    generators::DumbGenerator
->;
+    generators::DumbGenerator>;
 
 template <typename GeneratorType>
-class GeneratorTest : public ::testing::Test {
+class GeneratorTest : public ::testing::Test
+{
 public:
     using result_type = typename GeneratorType::result_type;
+
 protected:
     std::unique_ptr<GeneratorType> generator;
 
-    void SetUp() override {
+    void SetUp() override
+    {
         result_type seed = static_cast<result_type>(2567890);
         generator = std::make_unique<GeneratorType>(seed);
     }
-    void TearDown() override{
+    void TearDown() override
+    {
         generator.reset();
     }
 };
 
 template <typename ConceptType>
-class ConceptTest : public ::testing::Test {
+class ConceptTest : public ::testing::Test
+{
 public:
     using result_type = typename ConceptType::result_type;
+
 protected:
     std::unique_ptr<ConceptType> generator;
 
-    void SetUp() override {
+    void SetUp() override
+    {
         result_type seed = static_cast<result_type>(2567890);
         generator = std::make_unique<ConceptType>(seed);
     }
-    void TearDown() override{
+    void TearDown() override
+    {
         generator.reset();
     }
 };
 
 TYPED_TEST_SUITE(GeneratorTest, GeneratorTypes);
-TYPED_TEST(GeneratorTest, getRandomNumberTest) {
+TYPED_TEST(GeneratorTest, getRandomNumberTest)
+{
     EXPECT_NO_THROW(this->generator->getRandomNumber());
     EXPECT_NE(this->generator->getRandomNumber(),
-    this->generator->getRandomNumber());
+              this->generator->getRandomNumber());
     EXPECT_NE(this->generator->getRandomNumber(),
-    this->generator->getRandomNumber());
+              this->generator->getRandomNumber());
 }
 
-TYPED_TEST(GeneratorTest, minMaxValTest) {
+TYPED_TEST(GeneratorTest, minMaxValTest)
+{
     using result_type = typename TestFixture::result_type;
     EXPECT_LT(this->generator->min(), this->generator->max());
     auto min = std::numeric_limits<result_type>::min();
     auto max = std::numeric_limits<result_type>::max();
     EXPECT_EQ(this->generator->min(), min);
     EXPECT_EQ(this->generator->max(), max);
-    }
+}
 
 TYPED_TEST_SUITE(ConceptTest, UniformRandomNumberGeneratorConceptTypes);
-TYPED_TEST(ConceptTest, nothrow){
+TYPED_TEST(ConceptTest, nothrow)
+{
     using result_type = typename TestFixture::result_type;
 
     EXPECT_TRUE(true);
